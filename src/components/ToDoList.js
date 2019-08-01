9888e53c954e4353db9cc17445a64b88f131d3d7
Ania Blaziak <anblaziak@gmail.com>
2019-08-01 23:04:03 +0100

    style ToDoList

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,13 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 
+const StyledUl = styled.ul`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  line-height: 2;
+  font-size: 1.5em;
+`;
+
+const StyledLi = styled.li`
+  color: ${props => props.theme.body};
+  cursor: pointer;
+  &:hover {
+    color: grey;
+    text-decoration: line-through;
+  }
+`;
+
 const ToDoList = props => {
   return (
-    <ul>
+    <StyledUl>
       {props.items.map((item, i) => (
-        <li key={i}>{item}</li>
+        <StyledLi key={i}>{item}</StyledLi>
       ))}
-    </ul>
+    </StyledUl>
   );
 };
 
